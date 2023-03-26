62b149daed9117099331f1f58c1bb1e4d1f43c54
Nikhil Walia <nikhilwalia363@gmail.com>
2023-03-26 13:14:22 +0530

    DataBase Coonected

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require("express");
-
+const mongoose = require("mongoose");
 //Load Routes in Main App
 const musicRoutes = require("./Routes/music.js")
 const app = express();
@@ -9,4 +9,8 @@ app.get("/hello",(req,res)=>
 {
     return res.json({"msg":"hlo from backend"});
 })
+
+//Make DataBase Connection
+mongoose.connect(process.env.MONGO_CLUSTER).then(console.log("Database Connected"));
+
 app.listen(process.env.PORT,() => {console.log(`Server is listing on Port ${process.env.PORT}`)});
